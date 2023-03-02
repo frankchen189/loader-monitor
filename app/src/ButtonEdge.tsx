bf03d94b92c2675dbf7a6790fb3496461eec1f9c
Frank Chen <yihui.chen@tigergraph.com>
2023-03-01 16:53:08 -0800

    button edge;

---
+++ app/src/ButtonEdge.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { Position, getBezierPath } from 'reactflow';
+
+import './index.css';
+
+const foreignObjectSize = 40;
+
+const onEdgeClick = (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>, id: any) => {
+    evt.stopPropagation();
+    alert(`remove ${id}`);
+};
+
+export default function CustomEdge(
+    {
+        id,
+        sourceX,
+        sourceY,
+        targetX,
+        targetY,
+        sourcePosition,
+        targetPosition,
+        style = {},
+        markerEnd,
+    }: {
+        id: string,
+        sourceX: number,
+        sourceY: number,
+        targetX: number,
+        targetY: number,
+        sourcePosition: Position,
+        targetPosition: Position,
+        style: {},
+        markerEnd: string,
+    }
+) {
+    const [edgePath, labelX, labelY] = getBezierPath({
+        sourceX,
+        sourceY,
+        sourcePosition,
+        targetX,
+        targetY,
+        targetPosition,
+    });
+
+    const [isShown, setIsShown] = useState(false);
+
+    return (
+        <>
+            <path
+                id={id}
+                style={style}
+                className="react-flow__edge-path"
+                d={edgePath}
+                markerEnd={markerEnd}
+            />
+            <foreignObject
+                width={foreignObjectSize}
+                height={foreignObjectSize}
+                x={labelX - foreignObjectSize / 2}
+                y={labelY - foreignObjectSize / 2}
+                className="edgebutton-foreignobject"
+                requiredExtensions="http://www.w3.org/1999/xhtml"
+            >
+                <div>
+                    <button className="edgebutton" onClick={(event) => onEdgeClick(event, id)}>
+                        ?
+                    </button>
+
+
+                    {/* <button
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}>
+                    ?
+            </button>
+            {isShown && (
+                <div>
+                I'll appear when you hover over the button.
+                </div>
+            )} */}
+                </div>
+            </foreignObject>
+        </>
+    );
+}
--- app/src/Flow.tsx
+++ app/src/Flow.tsx
@@ -11,9 +11,22 @@ import ReactFlow, {
   MarkerType,
 } from "reactflow";
 
+import ButtonEdge from './ButtonEdge';
+
 import "reactflow/dist/style.css";
 
 const initialNodes: Node[] = [
+  // step 1
+  // {
+  //   id: '2',
+
+  //   data: { label: 'Group A' },
+  //   position: { x: 100, y: 100 },
+  //   className: 'light',
+  //   style: {
+  //     backgroundColor: 'rgba(255, 0, 0, 0.2)', width: 200, height: 200
+  //   },
+  // },
   {
     id: "1",
     type: "input",
@@ -43,29 +56,32 @@ const initialNodes: Node[] = [
     position: { x: 450, y: 200 }
   },
 
-  {
-    id: "41",
-    type: "output",
-    data: { label: `"Vertex Book" data objects` },
-    position: { x: 50, y: 300 }
-  },
-  {
-    id: "42",
-    type: "output",
-    data: { label: `"Vertex Price" data objects` },
-    position: { x: 250, y: 300 }
-  },
-  {
-    id: "43",
-    type: "output",
-    data: { label: `"Edge has" data objects` },
-    position: { x: 450, y: 300 }
-  },
+  // {
+  //   id: "41",
+  //   type: "output",
+  //   data: { label: `"Vertex Book" data objects` },
+  //   position: { x: 50, y: 300 }
+  // },
+  // {
+  //   id: "42",
+  //   type: "output",
+  //   data: { label: `"Vertex Price" data objects` },
+  //   position: { x: 250, y: 300 }
+  // },
+  // {
+  //   id: "43",
+  //   type: "output",
+  //   data: { label: `"Edge has" data objects` },
+  //   position: { x: 450, y: 300 }
+  // },
 ];
 
-const markerEnd = {
-  type: MarkerType.ArrowClosed,
-  color: '#000000',
+const edgeSettings = {
+  type: 'buttonedge',
+  markerEnd: {
+    type: MarkerType.ArrowClosed,
+    color: '#000000',
+  },
 }
 
 const initialEdges: Edge[] = [
@@ -73,47 +89,51 @@ const initialEdges: Edge[] = [
     id: "e1-2",
     source: "1",
     target: "2",
-    markerEnd: markerEnd
+    ...edgeSettings
   },
 
   {
     id: "e2-31",
     source: "2",
     target: "31",
-    markerEnd: markerEnd
+    ...edgeSettings
   },
   {
     id: "e2-32",
     source: "2",
     target: "32",
-    markerEnd: markerEnd
+    ...edgeSettings
   },
   {
     id: "e3-33",
     source: "2",
     target: "33",
-    markerEnd: markerEnd
+    ...edgeSettings
   },
 
-  {
-    id: "e31-41",
-    source: "31",
-    target: "41",
-    markerEnd: markerEnd
-  },
-  {
-    id: "e32-42",
-    source: "32",
-    target: "42",
-    markerEnd: markerEnd
-  },
-  {
-    id: "e33-43",
-    source: "33",
-    target: "43",
-    markerEnd: markerEnd
-  },
+  // { 
+  //     id: "e31-41", 
+  //     source: "31", 
+  //     target: "41",
+  //     ...edgeSettings
+  // },
+  // { 
+  //     id: "e32-42", 
+  //     source: "32", 
+  //     target: "42",
+  //     ...edgeSettings
+  // },
+  // { 
+  //     id: "e33-43", 
+  //     source: "33", 
+  //     target: "43",
+  //     ...edgeSettings
+  // },
 ];
+
+const edgeTypes = {
+  buttonedge: ButtonEdge,
+};
 
 const BasicFlow = () => {
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
@@ -132,6 +152,8 @@ const BasicFlow = () => {
       onNodesChange={onNodesChange}
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
+      edgeTypes={edgeTypes}
+      fitView
     >
 
       <Controls />
